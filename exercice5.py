5a8555f750f95f7b8ee86f15d04b4094f159f61a
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2021-02-03 05:49:00 -0500

    Update exercice5.py

--- exercice5.py
+++ exercice5.py
@@ -9,6 +9,7 @@ def points_perdu(dindon, lievre, cerf, ours_noir):
 if __name__ == '__main__':
 	dindon = int(input("Entrer le nombre de dindon: "))
 	lievre = int(input("Entrer le nombre de lievre: "))
-	cerf   = int(input("Entrer le nombre de cerf: "))
-	ours_noir   = int(input("Entrer le nombre d''ours noir: "))
+	cerf = int(input("Entrer le nombre de cerf: "))
+	ours_noir = int(input("Entrer le nombre d''ours noir: "))
 	points_perdu(dindon, lievre, cerf, ours_noir)
+	
